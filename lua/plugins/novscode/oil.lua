22e6d07d8aaaaadb236ad26cc45ae28c4239c207
Lasse Pladsen <lasse.pladsen@hotmail.com>
2025-03-23 11:04:47 +0100

    Oil file explorer

---
+++ lua/plugins/novscode/oil.lua
@@ -0,0 +1,20 @@
+return {
+	"stevearc/oil.nvim",
+	opts = {
+		watch_for_changes = true,
+		delete_to_trash = false,
+		default_file_explorer = true,
+		columns = {
+			"icon",
+			"permissions",
+			-- "size",
+			-- "mtime"
+		},
+		view_options = {
+			show_hidden = true,
+		},
+	},
+	keys = {
+		{"-", "<CMD>Oil<CR>", desc = "Open parent directory"},
+	},
+}
