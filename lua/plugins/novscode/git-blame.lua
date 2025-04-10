cb6cd62f127280bc75ec55809bd695b805faeda0
Lasse Pladsen <lasse.pladsen@hotmail.com>
2025-04-10 12:12:52 +0200

    Tweak blame signature. Add omni-preview with peek and csvview

--- lua/plugins/novscode/git-blame.lua
+++ lua/plugins/novscode/git-blame.lua
@@ -2,6 +2,6 @@ return {
     'f-person/git-blame.nvim',
 	opts = {
 		date_format = "%Y-%m-%d",
-		message_template = "<author>, <date> • <summary> • <sha>",
+		message_template = "<author>, <date> (<sha>) • <summary>",
 	},
 }
