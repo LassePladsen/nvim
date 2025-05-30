646d3fab38202b73a25baeff5cde1021a08c859b
Lasse Pladsen <lasse.pladsen@hotmail.com>
2025-05-30 21:21:18 +0200

    global func

--- lua/init.lua
+++ lua/init.lua
@@ -7,3 +7,14 @@ vim.cmd([[au TextYankPost * lua vim.highlight.on_yank {higroup="IncSearch", time
 -- vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 vim.g.have_nerd_font = true
+
+-- function that prints objects nicely
+function _G.put(...)
+  local objects = {}
+  for i = 1, select('#', ...) do
+    local v = select(i, ...)
+    table.insert(objects, vim.inspect(v))
+  end
+  print(table.concat(objects, '\n'))
+  return ...
+end
