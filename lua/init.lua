4bc2f1cd4efb4188eb14adfa82820602b55279da
Lasse Pladsen <lasse.pladsen@hotmail.com>
2025-06-09 21:31:26 +0200

    debugprint php override

--- lua/init.lua
+++ lua/init.lua
@@ -8,13 +8,36 @@ vim.cmd([[au TextYankPost * lua vim.highlight.on_yank {higroup="IncSearch", time
 
 vim.g.have_nerd_font = true
 
--- function that prints objects nicely
+-- function that prints objects nicely. NB: use := instead!
 function _G.put(...)
-  local objects = {}
-  for i = 1, select('#', ...) do
-    local v = select(i, ...)
-    table.insert(objects, vim.inspect(v))
-  end
-  print(table.concat(objects, '\n'))
-  return ...
+	local objects = {}
+	for i = 1, select("#", ...) do
+		local v = select(i, ...)
+		table.insert(objects, vim.inspect(v))
+	end
+	print(table.concat(objects, "\n"))
+	return ...
 end
+
+-- CUSTOM MACROS. Could use plugin debugprint instead
+-- local esc = vim.api.nvim_replace_termcodes("<ESC>", true, true, true)
+-- -- debug printing variable in visual selection for different filetypes
+-- -- js and ts
+-- vim.api.nvim_create_augroup("JSLogMacro", { clear = true })
+-- vim.api.nvim_create_autocmd("FileType", {
+-- 	group = "JSLogMacro",
+-- 	pattern = { "javascirpt", "typescript" },
+-- 	callback = function()
+-- 		vim.fn.setreg("l", "yoconsole.log('" .. esc .. "pa: ', " .. esc .. "pA);" .. esc)
+-- 	end,
+-- })
+
+-- -- php
+-- vim.api.nvim_create_augroup("PHPLogMacro", { clear = true })
+-- vim.api.nvim_create_autocmd("FileType", {
+-- 	group = "PHPLogMacro",
+-- 	pattern = { "php" },
+-- 	callback = function()
+-- 		vim.fn.setreg("l", "yoerror_log('" .. esc .. "pa: '" .. esc .. " . print_r(" .. esc .. "pa, true));" .. esc)
+-- 	end,
+-- })
--- lua/plugins/global/debugprint.lua
+++ lua/plugins/global/debugprint.lua
@@ -11,24 +11,39 @@ return {
 		"folke/snacks.nvim", -- Optional: If you want to use the `:Debugprint search` command with snacks.nvim
 	},
 
-	opts = {
-		keymaps = {
-			normal = {
-				toggle_comment_debug_prints = "g?c",
-				delete_debug_prints = "g?d",
-				search_debug_prints = "g?l",
-			},
-			insert = {
-				variable = "<C-G>v",
-			},
-			visual = {
-				variable_below = "g?v",
-				variable_above = "g?V",
-			},
-		},
-		-- … Other options
-	},
+	config = function()
+		local debugprint = require("debugprint")
 
+		debugprint.setup({
+			move_to_debugline = true,
+			display_counter = false,
+			print_tag = '',
+			highlight_lines = false,
+			keymaps = {
+				normal = {
+					toggle_comment_debug_prints = "g?c",
+					delete_debug_prints = "g?d",
+					search_debug_prints = "g?l",
+				},
+				insert = {
+					variable = "<C-G>v",
+				},
+				visual = {
+					variable_below = "g?v",
+					variable_above = "g?V",
+				},
+			},
+		})
+
+		debugprint.add_custom_filetypes({
+			["php"] = {
+				left = "error_log('",
+				right = "\\n', true));",
+				mid_var = "'. print_r(",
+				right_var = ", true));",
+			},
+		})
+	end,
 	lazy = false, -- Required to make line highlighting work before debugprint is first used
 	version = "*", -- Remove if you DON'T want to use the stable version
 }
