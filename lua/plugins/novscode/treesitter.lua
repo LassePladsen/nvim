3e6cb84c42a8a74a75053bf5916ea4a572c4ba3d
Lasse Pladsen <lasse.pladsen@hotmail.com>
2025-07-04 12:40:33 +0200

    Treesitter fix!

--- lua/plugins/novscode/treesitter.lua
+++ lua/plugins/novscode/treesitter.lua
@@ -1,10 +1,26 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    opts = {
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "php", "python", "typescript", "tsx", "json" },
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-    },
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"javascript",
+				"html",
+				"php",
+				"python",
+				"typescript",
+				"tsx",
+				"json",
+				"rust",
+			},
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+			auto_install = true,
+		})
+	end,
 }
