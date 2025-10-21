de70662622c401966589934369b3d8a844778952
Lasse Pladsen <lasse.pladsen@hotmail.com>
2025-10-21 14:17:32 +0200

    Fix: new lsp config

--- lua/plugins/novscode/lsp/nvim-lspconfig.lua
+++ lua/plugins/novscode/lsp/nvim-lspconfig.lua
@@ -288,17 +288,18 @@ return {
 		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
 		require("mason-lspconfig").setup({
-			-- handlers = { // OLD VERSION BEFORE NVIM LSP BREAKING CHANGE
-			-- 	function(server_name)
-			-- 		local server = servers[server_name] or {}
-			-- 		-- This handles overriding only values explicitly passed
-			-- 		-- by the server configuration above. Useful when disabling
-			-- 		-- certain features of an LSP (for example, turning off formatting for ts_ls)
-			-- 		server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-			-- 		vim.lsp.config(server_name).setup(server)
-			-- 	end,
-			-- },
-			ensure_installed = ensure_installed,
+			handlers = { -- OLD VERSION BEFORE NVIM LSP BREAKING CHANGE
+				function(server_name)
+					local server = servers[server_name] or {}
+					-- This handles overriding only values explicitly passed
+					-- by the server configuration above. Useful when disabling
+					-- certain features of an LSP (for example, turning off formatting for ts_ls)
+					server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+					vim.lsp.config[server_name] = server
+					vim.lsp.enable(server_name)
+				end,
+			},
+			-- ensure_installed = ensure_installed,
 		})
 	end,
 }
