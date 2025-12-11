ff8d9e5768a095a7365f425a636abcbe8e23d41e
Lasse Pladsen <lasse.pladsen@hotmail.com>
2025-12-11 19:42:31 +0100

    add toggleterm

---
+++ lua/plugins/novscode/toggleterm.lua
@@ -0,0 +1,11 @@
+return {
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		opts = {},
+		keys = {
+			{ "<leader>tt", "<CMD>ToggleTerm<CR>", mode = {"n", "i", "t", "v"} ,desc = "[T]oggle [T]erminal" },
+		},
+
+	},
+}
