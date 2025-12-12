5058250a788af13b9e862d3668c64c8a1a886e5f
Lasse Pladsen <lasse.pladsen@hotmail.com>
2025-12-12 13:30:57 +0100

    Fix: space lag in insert mode

--- lua/plugins/novscode/toggleterm.lua
+++ lua/plugins/novscode/toggleterm.lua
@@ -3,6 +3,6 @@ return {
 	version = "*",
 	opts = {},
 	keys = {
-		{ "<leader>tt", "<CMD>ToggleTerm<CR>", mode = { "n", "i", "t", "v" }, desc = "[T]oggle [T]erminal" },
+		{ "<leader>tt", "<CMD>ToggleTerm<CR>", mode = { "n",  "t", "v" }, desc = "[T]oggle [T]erminal" },
 	},
 }
