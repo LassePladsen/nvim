02c0aa0f804dd84fcd3572cc4d0c641fa1bd18d6
Lasse Pladsen <lasse.pladsen@hotmail.com>
2025-12-12 13:29:04 +0100

    fix format

--- lua/plugins/novscode/toggleterm.lua
+++ lua/plugins/novscode/toggleterm.lua
@@ -1,11 +1,8 @@
 return {
-	{
-		"akinsho/toggleterm.nvim",
-		version = "*",
-		opts = {},
-		keys = {
-			{ "<leader>tt", "<CMD>ToggleTerm<CR>", mode = {"n", "i", "t", "v"} ,desc = "[T]oggle [T]erminal" },
-		},
-
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	opts = {},
+	keys = {
+		{ "<leader>tt", "<CMD>ToggleTerm<CR>", mode = { "n", "i", "t", "v" }, desc = "[T]oggle [T]erminal" },
 	},
 }
